3a348924a77116333dfa4bbec7e52cca0b1a7c12
Chinnawat-TT <chinnawat.sangrung@gmail.com>
2023-12-17 22:29:43 +0700

    array and tuple

--- src/main.ts
+++ src/main.ts
@@ -34,3 +34,34 @@ let lastName = "Sangrung";
 // undefined & null ค่าว่างเปล่าที่ยังไม่ได้กำหนด
 // let a: undefined =undefined;
 // let b: null = null ;
+
+// TypeScript Arrays
+
+// const names:string[]=[]; 
+// names.push("toto")
+// // names.push(9) // ทำไม่ได้จะ error เเจ้งเตือน typeข้อมูลไม่ตรง
+// console.log(names)
+
+// readonly ใช้เพื่อป้องกันการเเก้ไขข้อมูล
+// const names: readonly string[]=["toto"]; 
+// names.push("OK") // push ข้อมูลเข้ามาไม่ได้
+// console.log(names) // toto
+
+// const num: readonly number[]=[1,2,3,4]; 
+// console.log(num) // [1,2,3,4]
+
+// Define our Tuple ตำแหน่ง index ต้องตรงกับประเภทของ type
+// let tuple:[number,boolean,string]
+// tuple=[10,true,"OK"]
+
+// let tuple: readonly [number,boolean,string]
+// tuple=[10,true,"OK"]
+// tuple.push("Typescript") // readonly เท่านั้น
+
+// // กำหนดชื่อได้
+// let tuple:[myNum:number,myBoo:boolean,myStr:string]=[10,true,"OK"]
+
+// destructuring
+// let tuple:[number,boolean,string]=[10,true,"OK"]
+// let [myNum,myBoo,myStr]=tuple
+// console.log(myNum,myBoo,myStr) // 10 true 'OK'
